68d056433b8918a65a115fa5a0d1c0d167d70c3b
Josh Clark <jgclark123@gmail.com>
2022-02-23 09:31:57 -0500

    Style tweaks and JS comments.
    Minor spacing adjustments.
    Fade out the copy button success/fail status.

--- logic.js
+++ logic.js
@@ -1,5 +1,8 @@
 var data = {};
 
+/**
+ * Fill the <select> with the list of users from the data object.
+ */
 function fillUserList(users) {
 	var select = document.querySelector("#whoami");
 	var fragment = new DocumentFragment();
@@ -14,6 +17,10 @@ function fillUserList(users) {
 	select.addEventListener("change", exportList);
 }
 
+/**
+ * Create the sortable list of games from the data object
+ * for the currently selected user.
+ */
 function createList(event) {
 	// Clear out existing gameList.
 	var gameList = document.querySelector("#gameList");
@@ -60,6 +67,7 @@ function createList(event) {
 			ol.appendChild(li);
 		});
 		
+		// Trigger the Sortable library to facilitate drag-drop.
 		Sortable.create(ol, {
 			animation: 150,
 			ghostClass: "ghost",
@@ -80,7 +88,7 @@ function exportList(event) {
 	var itemList = document.querySelector("#gameList").querySelectorAll(".list-group-item");
 	var exportText = "";
 	var i = 1;
-	const user = document.querySelector("#gameList h1").innerText;
+	const user = document.querySelector("#whoami").value;
 	
 	// Clear out data object for current user.
 	data[user] = [];
@@ -98,23 +106,33 @@ function exportList(event) {
 		});
 	});
 	
-	// Commit.
+	// Commit to the export textarea.
 	var exportField = document.querySelector('#exportField');
 	exportField.value = exportText;
+	
+	// Commit the data object to localStorage.
 	saveData();
 }
 
+/**
+ * Event handler for XHR to kick off loading the data object.
+ */
 function formatData(event) {
 	if (this.status === 200) {
 		data = this.response;
-		
 		fillUserList(Object.keys(data));
 	}
 	else {
-		console.log("Request failed.  " + this.status);
+		console.log("Request failed.  Status code: " + this.status);
 	}
 }
 
+/**
+ * Save the data object to localStorage.
+ * This fails silently if the user has not checked the consent checkbox.
+ *
+ * @TODO Also save to cookies in case localStorage is unavailable.
+ */
 function saveData() {
 	if (window.localStorage !== undefined && localStorage.getItem("cookie-consent") === "accepted") {
 		localStorage.setItem("data", JSON.stringify(data));
@@ -122,6 +140,11 @@ function saveData() {
 	//document.cookie = "data=" + document.querySelector('#exportField').value + "; max-age=31536000";
 }
 
+/**
+ * Load the data object from localStorage.
+ *
+ * @TODO Fallback to cookies if localStorage is unavailable.
+ */
 function loadData() {
 	if (window.localStorage !== undefined) {
 		data = JSON.parse(localStorage.getItem("data"));
@@ -132,11 +155,25 @@ function loadData() {
 	}
 }
 
+/**
+ * Clear the localStorage.
+ *
+ * @TODO Also clear cookies.
+ */
 function clearData() {
 	if (window.localStorage !== undefined) {
 		localStorage.clear();
 	}
 	//document.cookie = "data=; expires=0";
+}
+
+/**
+ * Restore the <div id="copyStatus"> to its initial, empty status.
+ */
+function clearCopyStatus() {
+	var status = document.querySelector("#copyStatus");
+	status.className = "";
+	status.innerText = "";
 }
 
 
@@ -176,11 +213,13 @@ document.querySelector("#copyButton").addEventListener("click", function (event)
 				var status = document.querySelector("#copyStatus");
 				status.className = "success";
 				status.innerText = "Success!  The text was copied to your clipboard.";
+				setTimeout(clearCopyStatus, 2500);
 			},
 			function () {
 				var status = document.querySelector("#copyStatus");
 				status.className = "failure";
 				status.innerText = "Your browser does not support the Clipboard API.  Copy it manually.";
+				setTimeout(clearCopyStatus, 5000);
 			}
 		);
 	}
